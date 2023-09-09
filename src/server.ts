5c6fbae9733bc93a7dce35c109f5c96bf0fb201d
Matheusz Nied <matheustae@hotmail.com>
2023-09-08 22:21:44 -0300

    CO4PO2
    add beach Model

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import './util/module-alias';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
-
+import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 export class SetupServer extends Server {
     constructor(private port = 3000) {
         super();
     }
 
-    public init(): void {
+    public async init(): Promise<void> {
         this.setupExpress();
         this.setupControllers();
+        await this.databaseSetup();
     }
 
     private setupExpress(): void {
@@ -20,7 +22,16 @@ export class SetupServer extends Server {
 
     private setupControllers(): void {
         const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
+        const beachesController = new BeachesController();
+        this.addControllers([forecastController, beachesController]);
+    }
+
+    private async databaseSetup(): Promise<void> {
+        await database.connect();
+    }
+
+    public async close(): Promise<void> {
+        await database.close();
     }
 
     public getApp(): Application {
